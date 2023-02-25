06c25d9e3625fe5486c8592bd45f82b2f0a2976c
Ranjit Thota <amailtoranjith@gmail.com>
2023-02-25 17:52:02 +0100

    final logics set

--- src/App.js
+++ src/App.js
@@ -20,53 +20,57 @@ function App() {
       title: "Iphone",
       price: 450,
       numinkart: 0,
+      src: "https://images.unsplash.com/photo-1616348436168-de43ad0db179?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1081&q=80",
     },
     {
       id: "2",
       title: "Macbook Pro",
       price: 900,
       numinkart: 0,
+      src: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1626&q=80",
     },
     {
       id: "3",
       title: "Ipad",
       price: 650,
       numinkart: 0,
+      src: "https://images.unsplash.com/photo-1544244015-0df4b3ffc6b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1715&q=80",
     },
     {
       id: "4",
       title: "Earphones",
       price: 90,
       numinkart: 0,
+      src: "https://images.unsplash.com/photo-1520186994231-6ea0019d8d51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1143&q=80",
     },
     {
       id: "5",
       title: "Camera",
       price: 900,
       numinkart: 0,
+      src: "https://images.unsplash.com/photo-1495121553079-4c61bcce1894?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=762&q=80",
     },
     {
       id: "6",
       title: "Camera lENS",
       price: 499,
       numinkart: 0,
+      src: "https://images.unsplash.com/photo-1582994254571-52c62d96ebab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
     },
   ]);
 
   return (
     <div className="App">
+      <h1>ElectroMarket</h1>
       <div className="products">
         {products.map((product, idx) => {
           return (
             <div className="product" key={product.id}>
-              <img
-                src="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1626&q=80"
-                alt="product"
-              ></img>
+              <img src={product.src} alt="product"></img>
               <h3>{product.title}</h3>
               <p>
                 {" "}
-                <b>{`${product.price} euros`}</b>
+                <b>{`${product.price} €`}</b>
               </p>
               <button
                 onClick={(e) => {
@@ -90,7 +94,7 @@ function App() {
         })}
       </div>
       <div>
-        <h2 className="kart-title">Kart</h2>
+        <h2 className="kart-heading">Kart</h2>
         {kart.length !== 0 ? (
           <div className="kart">
             <div className="kart-products">
@@ -98,8 +102,8 @@ function App() {
                 (product, idx) =>
                   product.numinkart > 0 && (
                     <div key={idx} className="kart-product">
-                      <h2>{product.title}</h2>
-                      <p>
+                      <h2 className="kart-title">{product.title}</h2>
+                      <div className="change-buttons">
                         <span>
                           <button
                             onClick={(e) => {
@@ -151,19 +155,19 @@ function App() {
                         >
                           +
                         </button>
-                      </p>
-                      <p>Price: ${product.price}</p>
+                      </div>
+                      <p className="price-para">Price: {product.price} €</p>
                     </div>
                   )
               )}
               <div className="kart-product">
                 <h2>Total</h2>
-                <p>Price: $ {kartPrice}</p>
+                <p>Price: {kartPrice} €</p>
               </div>
             </div>
           </div>
         ) : (
-          <div>Empty Kart</div>
+          <div className="kart">Kart is Empty, Add Items!</div>
         )}
       </div>
     </div>
